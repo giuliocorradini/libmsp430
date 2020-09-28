10287f43fe76c22cbf6fa86e4b5da68fe032444f
Giulio <39673120+giuliocorradini@users.noreply.github.com>
2020-09-28 14:22:10 +0200

    Moved libds

--- uart/uart.h
+++ uart/uart.h
@@ -1,6 +1,10 @@
 #include <msp430.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 enum UART_speed_t {bps9600=0, bps19200, bps38400, bps57600, bps115200, n_speeds};
 
 typedef struct __UART_t {
@@ -15,3 +19,6 @@ typedef struct __UART_t {
 void uart_global_config(enum UART_speed_t baudrate);
 void uart_init(UART* uc);
 
+#ifdef __cplusplus
+}
+#endif
